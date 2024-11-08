5b9ca77bfe7d333a6d1452e2c9cfd295beb4b3b9
muhammadanascode <anassohal34343@gmail.com>
2024-11-08 18:33:46 +0500

    Logic fixed

--- RowMajor.cpp
+++ RowMajor.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-    int n = 7;
-    int I[n] = {1, 2, 3, 4, 5, 6, 7}; // Example initialization for I
-    int S[n] = {1, 1, 1, 1, 1, 1, 1}; // Example initialization for S
+    int n = 3;
+    int I[n] = {1,2,3}; // Example initialization for I
+    int S[n] = {4,4,4}; // Example initialization for S
     int sum = 0;
 
     // Corrected indexing to start from 0 and stop before n
